cdfdcbd4a34e43bed051513286c928a4658f8a1d
LuisFernandoGuerrero <fernandoguerrero.free@gmail.com>
2023-04-26 11:48:06 -0500

    Cambios Proyectos

---
+++ app/components/proyectoscontenedor.jsx
@@ -0,0 +1,61 @@
+import imgProyecto from '../../public/img/ProyectoUno.png'
+
+const ProyectosContenedor = () => {
+    return (
+        <div className="container">
+            <div className="proyecto">
+                <div className="imgProy" data-text="LinFe"><img src={imgProyecto} alt="imagen proyecto 1" /></div>
+                <div className="infoProyecto">
+                    <div>
+                        <h3>LinFe</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, facere delectus eum possimus cum nemo ullam. Nemo officia nobis facilis optio sequi quis eveniet rem alias ex cupiditate. Fugit!</p>
+                        <a href="">Ver Más</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="proyecto">
+                <div className="imgProy" data-text="LinFe"><img src={imgProyecto} alt="imagen proyecto 1" /></div>
+                <div className="infoProyecto">
+                    <div>
+                        <h3>LinFe</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, facere delectus eum possimus cum nemo ullam. Nemo officia nobis facilis optio sequi quis eveniet rem alias ex cupiditate. Fugit!</p>
+                        <a href="">Ver Más</a>
+                    </div>
+                </div>
+            </div>
+            <div className="proyecto">
+                <div className="imgProy" data-text="LinFe"><img src={imgProyecto} alt="imagen proyecto 1" /></div>
+                <div className="infoProyecto">
+                    <div>
+                        <h3>LinFe</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, facere delectus eum possimus cum nemo ullam. Nemo officia nobis facilis optio sequi quis eveniet rem alias ex cupiditate. Fugit!</p>
+                        <a href="">Ver Más</a>
+                    </div>
+                </div>
+            </div>
+            <div className="proyecto">
+                <div className="imgProy" data-text="LinFe"><img src={imgProyecto} alt="imagen proyecto 1" /></div>
+                <div className="infoProyecto">
+                    <div>
+                        <h3>LinFe</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, facere delectus eum possimus cum nemo ullam. Nemo officia nobis facilis optio sequi quis eveniet rem alias ex cupiditate. Fugit!</p>
+                        <a href="">Ver Más</a>
+                    </div>
+                </div>
+            </div>
+            <div className="proyecto">
+                <div className="imgProy" data-text="LinFe"><img src={imgProyecto} alt="imagen proyecto 1" /></div>
+                <div className="infoProyecto">
+                    <div>
+                        <h3>LinFe</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, facere delectus eum possimus cum nemo ullam. Nemo officia nobis facilis optio sequi quis eveniet rem alias ex cupiditate. Fugit!</p>
+                        <a href="">Ver Más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProyectosContenedor
