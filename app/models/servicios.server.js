a3265d27d6e86779928e778aaef2105195f100e8
Luis Fernando Guerrero Cruz <fernandoguerrero.free@gmail.com>
2024-02-02 17:05:36 -0500

    Ultimos cambios variable entorno

--- app/models/servicios.server.js
+++ app/models/servicios.server.js
@@ -1,6 +1,6 @@
 export async function getServicios() {
     // const respuesta = await fetch(`${process.env.API_URL}servicios?populate=imagen`);
-    const respuesta = await fetch(`http://localhost:3000/servicios`);
+    const respuesta = await fetch(`${process.env.API_URL}servicios`);
     const resultado = await respuesta.json();
 
     return resultado;
